8eecaeb3f38cd235e05fbfc276b40ef57c9be7a8
hameethasheik <hameethasheik@DESKTOP-POH3H73>
2021-02-09 19:06:13 +0400

    This the commit done by Ms. Hameetha on 09th Feb 2021  06.28pm

---
+++ mavenproject1/src/main/java/com/muscatcollege/mavenproject1/Main.java
@@ -0,0 +1,20 @@
+package mvcexample;
+
+/**
+ *
+ * @author savi
+ */
+// Launcher for application
+//This is done at 7.05
+
+public class Main {
+   
+    public static void main(String args[]) {
+     
+        Model model = new Model();               // Create shared database
+        Controller1 c1 = new Controller1(model); // Create first controller
+        Controller2 c2 = new Controller2(model); // Create second controller
+        
+    } // main
+  
+} // Main
