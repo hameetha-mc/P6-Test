696a7842bd5cd992317814c4e53e0e4649a86c80
HAMEETHA SHEIK <HAMEETHA SHEIK@10.128.6.201>
2021-03-18 14:31:59 +0400

    This is the commit done by Hameetha on 18th March 2021

--- Main.java
+++ Main.java
@@ -14,7 +14,7 @@ public class Main {
         Model model = new Model();               // Create shared database
         Controller1 c1 = new Controller1(model); // Create first controller
         Controller2 c2 = new Controller2(model); // Create second controller
-        
+        System.out.println("Hello!Sheeba This is from Hameetha PC");
     } // main
   
 } // Main
